a91eb9dd70744d32a5b1d4dea2d3a1a09d6b921a
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2024-12-20 01:36:18 +0200

    finish adding new task login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-// https://github.com/ashrafsalem2/todo_app_front.git
+// inputs
+let taskInput = document.getElementById("task_input");
+let btn = document.getElementById("btn");
+
+// get the value from input throw click the btn
+btn.addEventListener("click", function () {
+  // console.log(taskInput.value);
+  var task = {
+    title: taskInput.value,
+    apiKey: "6764ac6160a208ee1fde73cd",
+  };
+  addTodoTask(task);
+});
+
+// add new todo element from todo api
+// first get the public key : "apiKey": "6764ac6160a208ee1fde73cd"
+async function addTodoTask(task) {
+  var data = await fetch('https://todos.routemisr.com/api/v1/todos', {
+    method: "post",
+    body: JSON.stringify(task),
+    headers: { "content-type": "application/json" },
+  });
+
+  var result = await data.json();
+  console.log(result);
+}
