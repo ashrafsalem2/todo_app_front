f6aca1fe7329eec55fff2db0b99dfe8f12452922
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2024-12-20 02:04:11 +0200

    finish display all tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // inputs
 let taskInput = document.getElementById("task_input");
 let btn = document.getElementById("btn");
+let tasks = document.querySelector(".tasks");
 
 // get the value from input throw click the btn
 btn.addEventListener("click", function () {
@@ -15,7 +16,7 @@ btn.addEventListener("click", function () {
 // add new todo element from todo api
 // first get the public key : "apiKey": "6764ac6160a208ee1fde73cd"
 async function addTodoTask(task) {
-  var data = await fetch('https://todos.routemisr.com/api/v1/todos', {
+  var data = await fetch("https://todos.routemisr.com/api/v1/todos", {
     method: "post",
     body: JSON.stringify(task),
     headers: { "content-type": "application/json" },
@@ -23,4 +24,38 @@ async function addTodoTask(task) {
 
   var result = await data.json();
   console.log(result);
+  if (result.message == "success") {
+    getAllTodos();
+  }
+}
+
+// get all todos from api
+async function getAllTodos() {
+  var data = await fetch(
+    "https://todos.routemisr.com/api/v1/todos/6764ac6160a208ee1fde73cd"
+  );
+  var result = await data.json();
+
+  if (result.message === "success") {
+    displayTodos(result);
+  }
+}
+//display all at the beg
+getAllTodos();
+
+// display all todo in html
+function displayTodos(result) {
+  var allTasks = ``;
+  for (var i = 0; i < result.todos.length; i++) {
+    allTasks += `
+             <div class="task d-flex justify-content-between w-75  rounded-2 px-3 py-2 m-auto my-3">
+                <div>${result.todos[i].title}</div>
+                <div>
+                    <i class="fa-regular fa-circle-check mx-3"></i>
+                    <i class="fa-solid fa-trash"></i>
+                </div>
+            </div>
+        `;
+  }
+  tasks.innerHTML = allTasks;
 }
